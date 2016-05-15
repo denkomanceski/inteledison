980feb0e4372dd5bcf1c6e35d6449b1464ad49b3
denkomanceski <denkomanceski@gmail.com>
2016-05-15 03:42:59 +0200

    changes

--- accessor.js
+++ accessor.js
@@ -6,9 +6,8 @@ var writeAtPin = function(pinNr, binary){
     myDigitalPin5.dir(mraa.DIR_OUT); //set the gpio direction to output
     myDigitalPin5.write(binary);
 };
-var myDigitalPin6 = new mraa.Gpio(6); //setup digital read on Digital pin #6 (D6)
-myDigitalPin6.dir(mraa.DIR_IN); //set the gpio direction to input
-periodicActivity(); //call the periodicActivity function
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
+var myDigitalPin6 = new mraa.Aio(2);//call the periodicActivity function
 function periodicActivity() //
 {
     var myDigitalValue =  myDigitalPin6.read(); //read the digital value of the pin
