fd0657e114b8eff95233dc7f6b174fd720c26a1a
denkomanceski <denkomanceski@gmail.com>
2016-05-15 03:19:52 +0200

    changes

---
+++ accessor.js
@@ -0,0 +1,11 @@
+var mraa = require('mraa');
+
+var iterate = function(pinNr){
+    var button = new mraa.Gpio(pinNr);     // set up digital read on digital pin #5<br>
+    button.dir(mraa.DIR_IN);           // set the GPIO direction to input<br><br>
+    var buttonState = button.read();   // read the value of the digital pin<br>
+    console.log(buttonState);
+};
+for(var i=0; i<16; i++){
+    iterate(i);
+}
